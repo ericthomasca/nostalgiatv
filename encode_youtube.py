c68d67d2740e27de6a6e0a86187dd64a1d436faf
Eric Thomas <eric@ericthomas.ca>
2022-07-21 23:07:17 -0230

    video download at 480p successfully

--- encode_youtube.py
+++ encode_youtube.py
@@ -6,15 +6,15 @@ import ffmpeg
 def main():
     # Get Videos
     yt_opts = {
-        'format': 'best',
+        'format': '22',  # 720p
         'outtmpl': 'videos/original/%(title)s.%(ext)s',
         'restrictfilenames': True
     }
 
     ytdl = yt_dlp.YoutubeDL(yt_opts)
 
+    # TODO change to argument
     video_link = ['https://www.youtube.com/watch?v=-qQ_HGJqaHQ']
-
     ytdl.download(video_link)
 
     # Convert Video
@@ -22,22 +22,19 @@ def main():
     encoded_dir = 'videos/encoded'
 
     for filename in os.listdir(original_dir):
-        input_video = os.path.join(original_dir, filename).replace('\\', '/')
-        output_video = os.path.join(encoded_dir, filename).replace('\\', '/')
-        print(input_video)
-        # output_video = os.path.join(encoded_dir, filename)
-
+        input_video = os.path.join(original_dir, filename)
+        # print(ffmpeg.probe(input_video))
+        output_video = os.path.join(encoded_dir, filename)
         (
             ffmpeg
             .input(input_video)
-            .filter('scale', 640, -1)
-            # .crop(107, 0, 640, 480)
-            # TODO figure out how to crop
+            .crop(160, 0, 960, 720)  # Remove black bars
+            .filter('scale', 640, -1)  # Add scaling when target resolution is known
             .output(output_video)
-            .run()
+            .run(overwrite_output=True)
         )
 
-    # Delete Originals
+        del input_video
 
 
 if __name__ == '__main__':
